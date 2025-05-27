ed8557029cf0b92e5c4e8cb028bac3336a7ed5e2
Fuzzsimp <jackson.fuesz@gmail.com>
2025-05-27 18:42:01 -0500

    Add files via upload

---
+++ hoist.py
@@ -0,0 +1,70 @@
+import base64
+
+#Loads input
+sample_string = input("enter text ")
+sample_string_bytes = sample_string.encode("ascii")
+
+#Encrypts input
+base64_bytes = base64.b64encode(sample_string_bytes)
+base64_string = base64_bytes.decode("ascii")
+
+print(f"Encoded string: {base64_string}") #Prints encryption
+
+#De-encryptes for verification
+decode_string_bytes = base64.b64decode(base64_bytes)
+decode_string = decode_string_bytes.decode("ascii")
+
+print(f"Decoded string: {decode_string}")
+
+
+#-----------------------------------------------
+
+#loads cypher
+def generate_key(msg, key):
+    key = list(key)
+    if len(msg) == len(key):
+        return key
+    else:
+        for i in range(len(msg) - len(key)):
+            key.append(key[i % len(key)])
+    return "".join(key)
+
+def encrypt_vigenere(msg, key):
+    encrypted_text = []
+    key = generate_key(msg, key)
+    for i in range(len(msg)):
+        char = msg[i]
+        if char.isupper():
+            encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('A')) % 26 + ord('A'))
+        elif char.islower():
+            encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('a')) % 26 + ord('a'))
+        else:
+            encrypted_char = char
+        encrypted_text.append(encrypted_char)
+    return "".join(encrypted_text)
+
+def decrypt_vigenere(msg, key):
+    decrypted_text = []
+    key = generate_key(msg, key)
+    for i in range(len(msg)):
+        char = msg[i]
+        if char.isupper():
+            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('A'))
+        elif char.islower():
+            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('a'))
+        else:
+            decrypted_char = char
+        decrypted_text.append(decrypted_char)
+    return "".join(decrypted_text)
+
+#Brings in BASE64 string
+text_to_encrypt = (f"{base64_string}")
+#Brings in key
+key = input("\n enter key: ")
+
+#Prints outputs
+encrypted_text = encrypt_vigenere(text_to_encrypt, key)
+print(f"Encrypted Text: {encrypted_text}")
+
+decrypted_text = decrypt_vigenere(encrypted_text, key)
+print(f"Decrypted Text: {decrypted_text}")
